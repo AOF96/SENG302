3c346ab8c82c94249cde92646dd4d0b09d3e8faa
Karna Malone <kkm22@uclive.ac.nz>
2020-03-09 22:59:13 +1300

    Added UI for edit email support #story[195]

--- client/src/components/Settings/UserEmailSettings.vue
+++ client/src/components/Settings/UserEmailSettings.vue
@@ -1,130 +1,155 @@
 <template>
 <div id="settingsWrap">
-    <UserSettingsMenu />
-    <div class="settingsContent">
-        <h1>Edit Email Settings</h1>
-        <hr>
-        <h3>Primary email</h3>
-        <div class="emailBlock">
-            <h4>{{primary_email}}</h4>
-        </div>
-        <h3>Secondary emails:</h3>
-        <div class="emailBlock emailSecondary" v-for="email in secondary_emails" v-bind:key="email">
-            <h4 @click="showEditBox = !showEditBox">{{email}}</h4>
-            <svg @click="showEditBox = !showEditBox" class="editIcon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-            <button class="setPrimaryButton" v-on:click="updatePrimaryEmail(email)">Set Primary</button>
-            <svg class="removeEmailButton" v-on:click="removeEmail(email)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-                <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
-                <path d="M0 0h24v24H0z" fill="none" /></svg>
-        </div>
-        <div v-if="this.secondary_emails.length < 4">
-          <form @submit.prevent>
-            <input id="addEmailInput" v-model="textInput" type="email" placeholder="Enter new email (Up to 4)" required>
-            <button id="addEmailButton" v-if="showButton" v-on:click="addEmail(textInput)">Add</button>
-          </form>
-        </div>
+  <UserSettingsMenu />
+  <div class="settingsContent">
+    <h1>Edit Email Settings</h1>
+    <hr>
+    <h3>Primary email</h3>
+    <div class="emailBlock">
+      <h4>{{primary_email}}</h4>
     </div>
-    <transition name="fade">
-        <div id="cover" v-if="showEditBox" @click="showEditBox = !showEditBox"></div>
-    </transition>
-    <transition name="fadeup">
-        <div class="floatingBox" id="editEmailBox" v-if="showEditBox">
-
-        </div>
-    </transition>
+    <h3>Secondary emails:</h3>
+    <div class="emailBlock emailSecondary" v-for="email in secondary_emails" v-bind:key="email">
+      <h4 v-on:click="openEmailEditBox(email)">{{email}}</h4>
+      <svg v-on:click="openEmailEditBox(email)" class="editIcon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+        <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+        <path d="M0 0h24v24H0z" fill="none" /></svg>
+      <button class="setPrimaryButton" v-on:click="updatePrimaryEmail(email)">Set Primary</button>
+      <svg class="removeEmailButton" v-on:click="removeEmail(email)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+        <path d="M0 0h24v24H0z" fill="none" /></svg>
+    </div>
+    <div v-if="this.secondary_emails.length < 4">
+      <form @submit.prevent>
+        <input id="addEmailInput" v-model="textInput" type="email" placeholder="Enter new email (Up to 4)" required>
+        <button id="addEmailButton" v-if="showButton" v-on:click="addEmail(textInput)">Add</button>
+      </form>
+    </div>
+  </div>
+  <transition name="fade">
+    <div id="cover" v-if="showEditBox" @click="showEditBox = false"></div>
+  </transition>
+  <transition name="fadeup">
+    <div class="floatingBox" id="editEmailBox" v-if="showEditBox">
+      <h1>Edit Email</h1>
+      <form @submit.prevent>
+        <input id="editEmailInput" v-model="editEmailInput" type="email" placeholder="Secondary Email" required>
+        <button id="editEmailButton" v-on:click="editEmail()">Update</button>
+      </form>
+    </div>
+  </transition>
 </div>
 </template>
 
 <script>
 import UserSettingsMenu from '@/components/Settings/UserSettingsMenu'
 import {
-    userInfo
+  userInfo
 } from "../../globals";
 import axios from "axios";
 const SERVER_URL = 'https://4967d4f4-8301-42d1-a778-e3d150633644.mock.pstmn.io';
 
 export default {
-    components: {
-        UserSettingsMenu
-    },
-    data() {
-        return {
-            primary_email: userInfo.email,
-            secondary_emails: userInfo.secondaryEmails,
-            showButton: true,
-            textInput: '',
-            newEmail: '',
-            showEditBox: false
-        }
-    },
-    methods: {
-        updateGlobals() {
-            userInfo.email = this.primary_email;
-            userInfo.secondaryEmails = this.secondary_emails;
-        },
-
-        /*
-           Adds a new email into the secondary emails lists. Prevents the user from entering empty text or from trying to
-           enter an existing email.
-        */
-        addEmail(textInput) {
-            if (this.secondary_emails.includes(textInput) || textInput == this.primary_email) {
-                alert("Please enter an email that has not been used before");
-            } else if(textInput != "" && (/[^\s]+@[^\s]+/.test(textInput))) {
-                axios.post(SERVER_URL + '/editemail', {
-                        new_email: textInput
-                    })
-                    .then((response) => {
-                        console.log(response.data.msg3);
-                    }, (error) => {
-                        console.log(error);
-                    });
-
-                if (this.secondary_emails.length === 4) {
-                    alert("You already have 5 emails, delete one if you want to add more.");
-                    this.showButton = false;
-                    return;
-                }
-                this.secondary_emails.push(this.textInput);
-                this.updateGlobals();
-                var tempThis = this;
-                setTimeout(function() {
-                  tempThis.textInput = "";
-                }, 10);
-
-            }
-        },
-
-        /*
-         Function that updates the primary email of an user by picking one from the secondary emails list and adding the
-         previous primary email to the secondary list.
-         */
-        updatePrimaryEmail(secondaryEmail) {
-            const index = this.secondary_emails.indexOf(secondaryEmail);
-            if (index > -1) {
-                this.secondary_emails.splice(index, 1);
-            }
-            this.secondary_emails.push(this.primary_email);
-            this.primary_email = secondaryEmail;
-            this.updateGlobals();
-        },
-
-        /* Function that removes an email from the secondary emails list */
-        removeEmail(email) {
-            if (this.secondary_emails.length === 1) {
-                this.secondary_emails = []
-            } else {
-                const index = this.secondary_emails.indexOf(email);
-                if (index > -1) {
-                    this.secondary_emails.splice(index, 1);
-                }
-            }
-            if (this.secondary_emails.length > 0) {
-                this.showButton = true;
-            }
-
-            this.updateGlobals();
-        }
+  components: {
+    UserSettingsMenu
+  },
+  data() {
+    return {
+      primary_email: userInfo.email,
+      secondary_emails: userInfo.secondaryEmails,
+      showButton: true,
+      textInput: '',
+      newEmail: '',
+      tempOldEmail: '',
+      editEmailInput: '',
+      showEditBox: false
     }
+  },
+  methods: {
+    updateGlobals() {
+      userInfo.email = this.primary_email;
+      userInfo.secondaryEmails = this.secondary_emails;
+    },
+
+    /*
+       Adds a new email into the secondary emails lists. Prevents the user from entering empty text or from trying to
+       enter an existing email.
+    */
+    addEmail(textInput) {
+      if (this.secondary_emails.includes(textInput) || textInput == this.primary_email) {
+        alert("Please enter an email that has not been used before");
+      } else if (textInput != "" && (/[^\s]+@[^\s]+/.test(textInput))) {
+        axios.post(SERVER_URL + '/editemail', {
+            new_email: textInput
+          })
+          .then((response) => {
+            console.log(response.data.msg3);
+          }, (error) => {
+            console.log(error);
+          });
+
+        if (this.secondary_emails.length === 4) {
+          alert("You already have 5 emails, delete one if you want to add more.");
+          this.showButton = false;
+          return;
+        }
+        this.secondary_emails.push(this.textInput);
+        this.updateGlobals();
+        var tempThis = this;
+        setTimeout(function() {
+          tempThis.textInput = "";
+        }, 10);
+
+      }
+    },
+
+    /*
+     Function that updates the primary email of an user by picking one from the secondary emails list and adding the
+     previous primary email to the secondary list.
+     */
+    updatePrimaryEmail(secondaryEmail) {
+      const index = this.secondary_emails.indexOf(secondaryEmail);
+      if (index > -1) {
+        this.secondary_emails.splice(index, 1);
+      }
+      this.secondary_emails.push(this.primary_email);
+      this.primary_email = secondaryEmail;
+      this.updateGlobals();
+    },
+
+    openEmailEditBox(secondaryEmail) {
+      this.tempOldEmail = secondaryEmail;
+      this.editEmailInput = secondaryEmail;
+      this.showEditBox = true;
+    },
+
+    editEmail() {
+      if (/[^\s]+@[^\s]+/.test(this.editEmailInput)) {
+        const index = this.secondary_emails.indexOf(this.tempOldEmail);
+        this.secondary_emails[index] = this.editEmailInput;
+        this.showEditBox = false;
+        //
+        //  Send update to server
+        //
+      }
+    },
+
+    /* Function that removes an email from the secondary emails list */
+    removeEmail(email) {
+      if (this.secondary_emails.length === 1) {
+        this.secondary_emails = []
+      } else {
+        const index = this.secondary_emails.indexOf(email);
+        if (index > -1) {
+          this.secondary_emails.splice(index, 1);
+        }
+      }
+      if (this.secondary_emails.length > 0) {
+        this.showButton = true;
+      }
+
+      this.updateGlobals();
+    }
+  }
 }
 </script>
