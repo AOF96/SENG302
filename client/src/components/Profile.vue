0fbe492c17d995b0d33da4a7f699f2872b8ded24
Samuel Sandri <sjs234@uclive.ac.nz>
2020-04-30 16:05:05 +1200

    Moving profile info to sidebars
    Implemented sidebars on the profile page to show user information. Adjusted country info style.
    
    #story[253]

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -4,104 +4,110 @@
       <div id="profileBanner">
       </div>
       <div id="profileWrap">
-        <div id="profilePublicInfo">
-          <svg id="profileUserIcon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v1c0 .55.45 1 1 1h14c.55 0 1-.45 1-1v-1c0-2.66-5.33-4-8-4z"/></svg>
-          <div id="userQuickInfoWrap">
-            <h1 id="userName">{{ user.firstname }} {{user.middlename}} {{ user.lastname }} <span id="userNickname">({{ user.nickname }})</span></h1>
-            <h2 id="userFitnessLevel">Fitness Level: {{ fitnessDict[user.fitness] }}</h2>
+        <div id="sidebarLeft">
+          <div class="profileInfo">
+            <table id ="profileTable">
+              <tr>
+                <td class="profileTableTd" col width = "150">Gender:</td>
+                <td class="profileTableTd">{{ user.gender }}</td>
+              </tr>
+              <tr>
+                <td class="profileTableTd">DOB:</td>
+                <td class="profileTableTd">{{ user.date_of_birth }}</td>
+              </tr>
+              <tr>
+                <td class="profileTableTd">Primary Email:</td>
+                <td class="profileTableTd">{{ user.primary_email }}</td>
+              </tr>
+              <tr>
+                <td class="profileTableTd">Bio:</td>
+                <td class="profileTableTd">{{ user.bio }}</td>
+              </tr>
+              <tr>
+                <td class="profileTableTd">Secondary Emails:</td>
+                <div style="margin-top: 4px" v-for="email in user.additional_email" v-bind:key="email">
+                  <p >{{email}}</p>
+                </div>
+              </tr>
+              <tr>
+                <td class="profileTableTd">Activities:</td>
+                <td class="profileTableTd">{{ user.activities }}</td>
+              </tr>
+              <tr>
+                <td class="profileTableTd">Continuous Activities:</td>
+                <td class="profileTableTd">{{ user.cont_activities }}</td>
+              </tr>
+              <tr>
+                <td class="profileTableTd">Duration Activities:</td>
+                <td class="profileTableTd">{{ user.dur_activities }}</td>
+              </tr>
+            </table>
+            <button type="button" onclick="document.getElementById('myForm').style.display = 'block'">Add Activity</button>
           </div>
-          <router-link to="/settings/profile" id="profileEditButton">Edit profile</router-link>
-          <div class="floatClear"></div>
         </div>
-        <PassportCountries/>
-        <div class="profileInfo">
-          <table id ="profileTable">
-            <tr>
-              <td class="profileTableTd" col width = "150">Gender:</td>
-              <td class="profileTableTd">{{ user.gender }}</td>
-            </tr>
-            <tr>
-              <td class="profileTableTd">DOB:</td>
-              <td class="profileTableTd">{{ user.date_of_birth }}</td>
-            </tr>
-            <tr>
-              <td class="profileTableTd">Primary Email:</td>
-              <td class="profileTableTd">{{ user.primary_email }}</td>
-            </tr>
-            <tr>
-              <td class="profileTableTd">Bio:</td>
-              <td class="profileTableTd">{{ user.bio }}</td>
-            </tr>
-            <tr>
-              <td class="profileTableTd">Secondary Emails:</td>
-              <div style="margin-top: 4px" v-for="email in user.additional_email" v-bind:key="email">
-                <p >{{email}}</p>
-              </div>
-            </tr>
-            <tr>
-              <td class="profileTableTd">Activities:</td>
-              <td class="profileTableTd">{{ user.activities }}</td>
-            </tr>
-            <tr>
-              <td class="profileTableTd">Continuous Activities:</td>
-              <td class="profileTableTd">{{ user.cont_activities }}</td>
-            </tr>
-            <tr>
-              <td class="profileTableTd">Duration Activities:</td>
-              <td class="profileTableTd">{{ user.dur_activities }}</td>
-            </tr>
-          </table>
-          <button type="button" onclick="document.getElementById('myForm').style.display = 'block'">Add Activity</button>
-        </div>
-
-        <div class="form-popup" id="myForm">
-          <form action="/action_page.php" class="form-container">
-            <h1>New Activity</h1>
-
-            <label for="name"><b>Activity Name</b></label>
-            <input type="text" id="name" required>
-
-            <label for="time">Continuous?</label>
-            <select id="time">
-              <option value="continuous">Continuous</option>
-              <option value="duration">Duration</option>
-            </select>
-
-            <label for="start_date">Start Date</label>
-            <input type = "date" id="start_date">
-
-            <label for="end_date">End Date</label>
-            <input type = "date" id="end_date">
-
-            <label for="start_time">Start Time</label>
-            <input type = "time" id="start_time">
-
-            <label for="end_time">End Time</label>
-            <input type = "time" id="end_time">
-
-            <label for="desc"><b>Description</b></label>
-            <input type="text" id="desc">
-
-            <label><b>Location</b></label>
-            <div>
-            <select v-model="adding_country"
-                    name="passportCountries"
-                    placeholder="Passport Countries"
-                    value="Passport Countries"
-                    required>
-              <option selected disabled hidden>Passport Countries</option>
-              <option v-for="addingCountry in countries_option" v-bind:key="addingCountry">
-                {{addingCountry}}
-              </option>
-            </select>
+        <div id="centreColumn">
+          <div id="profilePublicInfo">
+            <svg id="profileUserIcon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v1c0 .55.45 1 1 1h14c.55 0 1-.45 1-1v-1c0-2.66-5.33-4-8-4z"/></svg>
+            <div id="userQuickInfoWrap">
+              <h1 id="userName">{{ user.firstname }} {{user.middlename}} {{ user.lastname }} <span id="userNickname">({{ user.nickname }})</span></h1>
+              <h2 id="userFitnessLevel">Fitness Level: {{ fitnessDict[user.fitness] }}</h2>
             </div>
+            <router-link to="/settings/profile" id="profileEditButton">Edit profile</router-link>
+            <div class="floatClear"></div>
+          </div>
+
+          <div class="form-popup" id="myForm">
+            <form action="/action_page.php" class="form-container">
+              <h1>New Activity</h1>
+
+              <label for="name"><b>Activity Name</b></label>
+              <input type="text" id="name" required>
+
+              <label for="time">Continuous?</label>
+              <select id="time">
+                <option value="continuous">Continuous</option>
+                <option value="duration">Duration</option>
+              </select>
+
+              <label for="start_date">Start Date</label>
+              <input type = "date" id="start_date">
+
+              <label for="end_date">End Date</label>
+              <input type = "date" id="end_date">
+
+              <label for="start_time">Start Time</label>
+              <input type = "time" id="start_time">
+
+              <label for="end_time">End Time</label>
+              <input type = "time" id="end_time">
+
+              <label for="desc"><b>Description</b></label>
+              <input type="text" id="desc">
+
+              <label><b>Location</b></label>
+              <div>
+              <select v-model="adding_country"
+                      name="passportCountries"
+                      placeholder="Passport Countries"
+                      value="Passport Countries"
+                      required>
+                <option selected disabled hidden>Passport Countries</option>
+                <option v-for="addingCountry in countries_option" v-bind:key="addingCountry">
+                  {{addingCountry}}
+                </option>
+              </select>
+              </div>
 
 
-            <button type="submit" class="btn">Create</button>
-            <button type="submit" class="btn cancel" onclick="document.getElementById('myForm').style.display = 'none'">Close</button>
-          </form>
+              <button type="submit" class="btn">Create</button>
+              <button type="submit" class="btn cancel" onclick="document.getElementById('myForm').style.display = 'none'">Close</button>
+            </form>
+          </div>
+
         </div>
-
+        <div id="sidebarRight">
+          <PassportCountries/>
+        </div>
       </div>
     </div>
 </template>
@@ -190,10 +196,6 @@
   }
 
   /* Green */
-  .success {
-    color: green;
-  }
-
   .btn:hover {
     background-color: #4CAF50;
     color: white;
@@ -203,18 +205,9 @@
     padding-left: 10px;
   }
 
-  .w3- img {
-
+  button {
+      margin-left: 8px;
   }
-
-  #input-field {
-       width: 300px;
-       margin-top: 15px;
-   }
-
-    button {
-        margin-left: 8px;
-    }
 
   .profileInfo {
       width: 100%;
@@ -222,12 +215,6 @@
       background: white;
       box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.20);
       border-radius: 14px;
-      font-family: Roboto;
-      margin-top: 15px;
+    font-family: Roboto;
   }
-
-    .profileInfoP {
-        padding: 1px 10px;
-        font-family: Roboto;
-    }
 </style>
