b2242742b7902ac84bedbee4d5874ffb667a9ce9
Karna Malone <kkm22@uclive.ac.nz>
2020-05-02 13:48:24 +1200

    Added functionality for loading user data based on the url variable "u"
    #story[251] #pair[sga113, kkm22]

--- client/src/components/Settings/UserProfileSettings.vue
+++ client/src/components/Settings/UserProfileSettings.vue
@@ -8,22 +8,22 @@
         <h6 class="edit_success" id="success" hidden="true"></h6>
         <form @submit.prevent>
             <h2>First Name</h2>
-            <input class="editProfileInput" type="text" name="fname" v-model="user.firstname" placeholder="First Name*" required>
+            <input class="editProfileInput" type="text" name="fname" v-model="searchedUser.firstname" placeholder="First Name*" required>
             <h2>Middle Name</h2>
-            <input class="editProfileInput" type="text" name="lname" v-model="user.middlename" placeholder="Middle Name">
+            <input class="editProfileInput" type="text" name="lname" v-model="searchedUser.middlename" placeholder="Middle Name">
             <h2>Last Name</h2>
-            <input class="editProfileInput" type="text" name="lname" v-model="user.lastname" placeholder="Last Name*" required>
+            <input class="editProfileInput" type="text" name="lname" v-model="searchedUser.lastname" placeholder="Last Name*" required>
             <h2>Nickname</h2>
-            <input class="editProfileInput" type="text" name="nickname" v-model="user.nickname" placeholder="Nickname">
+            <input class="editProfileInput" type="text" name="nickname" v-model="searchedUser.nickname" placeholder="Nickname">
             <h2>Gender</h2>
-            <select class="editProfileInput editProfileInputGender" v-model="user.gender" name="gender" placeholder="Gender" value="Gender" required>
+            <select class="editProfileInput editProfileInputGender" v-model="searchedUser.gender" name="gender" placeholder="Gender" value="Gender" required>
                 <option selected disabled hidden>Gender</option>
                 <option>Non-Binary</option>
                 <option>Female</option>
                 <option>Male</option>
             </select>
             <h2>Fitness Level</h2>
-            <select class="editProfileInput editProfileInputGender" v-model="user.fitness" name="fitnesslevel" placeholder="fitness" value="fitness" required>
+            <select class="editProfileInput editProfileInputGender" v-model="searchedUser.fitness" name="fitnesslevel" placeholder="fitness" value="fitness" required>
                 <option value = 0>I never exercise</option>
                 <option value = 1>I can walk a short distance</option>
                 <option value = 2>I can jog a short distance</option>
@@ -31,9 +31,9 @@
                 <option value = 4>I can run a marathon</option>
             </select>
             <h2>Birthday</h2>
-            <input v-model="user.date_of_birth" class="editProfileInput" name="birthday" type="date" required>
+            <input v-model="searchedUser.date_of_birth" class="editProfileInput" name="birthday" type="date" required>
             <h2>Bio</h2>
-            <textarea maxlength="255" class="editProfileTextarea" name="bio" v-model="user.bio" placeholder="Write about yourself"></textarea>
+            <textarea maxlength="255" class="editProfileTextarea" name="bio" v-model="searchedUser.bio" placeholder="Write about yourself"></textarea>
             <button id="settingsProfileSubmit" v-on:click="updateProfile()" type="submit">Update Profile</button>
         </form>
     </div>
@@ -54,19 +54,24 @@ export default {
     computed: {
         ...mapGetters(['user'])
     },
+    data: function() {
+      return {
+        searchedUser: {}
+      }
+    },
     methods: {
         ...mapActions(['logout']),
         ...mapActions(['updateUserProfile']),
 
         /*
-            Sends a request to the server side to update the user's profile info. Displays error messages if the update
+            Sends a request to the server side to update the searchedUser's profile info. Displays error messages if the update
             was unsuccessful.
          */
         updateProfile() {
-            console.log(this.user.fitness);
-            apiUser.editProfile(this.user.profile_id, this.user.firstname, this.user.lastname, this.user.middlename,
-                this.user.nickname, this.user.primary_email, this.user.bio, this.user.date_of_birth, this.user.gender,
-                Number(this.user.fitness), this.user.additional_email, this.user.passports).then((response) => {
+            console.log(this.searchedUser.fitness);
+            apiUser.editProfile(this.searchedUser.profile_id, this.searchedUser.firstname, this.searchedUser.lastname, this.searchedUser.middlename,
+                this.searchedUser.nickname, this.searchedUser.primary_email, this.searchedUser.bio, this.searchedUser.date_of_birth, this.searchedUser.gender,
+                Number(this.searchedUser.fitness), this.searchedUser.additional_email, this.searchedUser.passports).then((response) => {
                 this.updateUserProfile(this.user);
                 document.getElementById("success").hidden = false;
                 document.getElementById("success").innerText = "Updated Successfully";
@@ -81,11 +86,25 @@ export default {
         },
 
         /*
-           Increases the number of passports a user owns.
+            Uses user id from url to request user data.
          */
-        addPassportCountries() {
-            this.passportCountries.num_of_countries++;
-        },
+        async loadSearchedUser() {
+          if(this.$route.query.u == null){
+            this.$router.push('/settings/profile?u='+this.user.profile_id);
+            this.searchedUser = this.user;
+          }else{
+            var tempUserData = await apiUser.getUserById(this.$route.query.u);
+            if(tempUserData == "Invalid permissions"){
+              this.$router.push('/settings/profile?u='+this.user.profile_id);
+              this.searchedUser = this.user;
+            }else{
+              this.searchedUser = tempUserData;
+            }
+          }
+        }
+    },
+    mounted() {
+      this.loadSearchedUser();
     }
 }
 </script>
