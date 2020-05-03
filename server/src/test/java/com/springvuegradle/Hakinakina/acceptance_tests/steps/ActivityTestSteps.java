5f6cae7e513cda41cb6c15140a8dc24207b55331
Adrian Osuna Fregoso <aos26@uclive.ac.nz>
2020-05-03 22:23:52 +1200

    Implemented cucumber tests for deleting activities.
    -Implemented a user scenario that involves creating a new user, creating an activity and deleting the created activity.
    
    #story[253]

--- server/src/test/java/com/springvuegradle/Hakinakina/acceptance_tests/steps/ActivityTestSteps.java
+++ server/src/test/java/com/springvuegradle/Hakinakina/acceptance_tests/steps/ActivityTestSteps.java
@@ -1,21 +1,71 @@
 package com.springvuegradle.Hakinakina.acceptance_tests.steps;
 
-import com.springvuegradle.Hakinakina.entity.Activity;
-import com.springvuegradle.Hakinakina.entity.ActivityRepository;
-import com.springvuegradle.Hakinakina.entity.UserRepository;
+import com.springvuegradle.Hakinakina.controller.ActivityController;
+import com.springvuegradle.Hakinakina.controller.ActivityService;
+import com.springvuegradle.Hakinakina.controller.UserService;
+import com.springvuegradle.Hakinakina.entity.*;
+import com.springvuegradle.Hakinakina.util.ResponseHandler;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@WebMvcTest(User.class)
+@AutoConfigureWebMvc
 public class ActivityTestSteps {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private MvcResult result;
+    @Mock
     private ActivityRepository activityRepository;
+    @Mock
     private UserRepository userRepository;
+    @Mock
+    private ActivityService activityService;
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private ActivityController activityController;
+
+    private User user1;
+    Session session1 = new Session("t0k3n");
+    Activity activity1;
+
+    @Before
+    public void setup()
+    {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(activityController).build();
+    }
 
     @Before
     public void resetLocalFields() throws SQLException {
@@ -36,4 +86,41 @@ public class ActivityTestSteps {
     public void exactlyActivityShouldBeReturned(int expectedNumOfAcitivities) {
         assertTrue(true);
     }
+
+
+    @Given("I create an account with email {string}")
+    public void iCreateAccountWithEmail(String email) throws Exception {
+        user1 = new User("John", "Doe", email, "1985-12-20", Gender.MALE, 4, "password");
+        assertEquals(email, user1.getPrimaryEmail());
+        user1.setUserId((long) 1);
+        session1.setUser(user1);
+    }
+
+    @And("I create an activity with name {string}, description {string} and ID {long}")
+    public void i_create_an_activity(String name, String description, long id) throws Exception {
+        activity1 = new Activity(name, description,
+                true, new Date(2021, 10, 10), new Date(2021, 10, 11),
+                "Park");
+
+        activity1.setId(id);
+        Set<ActivityType> activityTypes = new HashSet<ActivityType>();
+        activityTypes.add(new ActivityType("Fun"));
+        activity1.setActivityTypes(activityTypes);
+
+        Activity newActivity = activityRepository.save(activity1);
+        activityRepository.insertActivityForUser((long) 1, id);
+
+        assertEquals(name, activity1.getName());
+    }
+
+    @When("I delete the created activity")
+    public void i_delete_the_created_activity() {
+        activityService.removeActivity(user1.getUserId(), activity1.getId(), "t0k3n");
+    }
+
+    @Then("The created activity with ID {long} no longer exists")
+    public void activityNoLongerExists(long ID) {
+        assertNull(activityRepository.findActivityById(ID));
+    }
+
 }
