70037c97e640211b740399c8acf9dbd567c5f440
Frankie <fop12@uclive.ac.nz>
2020-05-06 00:20:08 +1200

    Fixed some logic for deleting activity
    -Calls the endpoint but is currently missing the activityId for the endpoint
    
    #story[253]

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -44,6 +44,7 @@
             <h3 style="display: inline-block">Duration Activities:</h3>
             <div class="activitySummaryDiv" v-for="activity in user.dur_activities" v-bind:key="activity">
               {{activity.name}}<br> <h2 class="userFitnessLevel">{{activity.description}}</h2>
+
             </div>
             <hr>
             <h3 style="display: inline-block">Continuous Activities:</h3>
@@ -52,7 +53,6 @@
               {{activity.name}}<br> <h2 class="userFitnessLevel">{{activity.description}}</h2>
               <button id="deleteActivityButton" type="button" v-on:click="deleteContinuousActivity(activity)">Delete Activity</button>
             </div>
-\
           </div>
 
         </div>
@@ -120,19 +120,23 @@
       },
     methods: {
       ...mapActions(['updatePassports']),
+      ...mapActions(['updateUserDurationActivities']),
+      ...mapActions(['updateUserContinuousActivities']),
+
       startUp() {
         console.log('init')
         this.user.passports = this.user.passports.slice()
       },
       deleteDurationActivity(activity) {
-        let index = this.user.cont_activities.indexOf(activity);
-        this.user.cont_activities.splice(index, 1);
-        apiActivity.deleteActivity(this.activity.author_id, this.$route.params.activityId);
-      },
-      deleteContinuousActivity(activity) {
         let index = this.user.dur_activities.indexOf(activity);
         this.user.dur_activities.splice(index, 1);
-        apiActivity.deleteActivity(this.activity.author_id, this.$route.params.activityId);
+        apiActivity.deleteActivity(this.user.profile_id, this.$route.params.activityId);
+      },
+      deleteContinuousActivity(activity) {
+        let index = this.user.cont_activities.indexOf(activity);
+        this.user.cont_activities.splice(index, 1);
+        apiActivity.deleteActivity(this.user.profile_id, this.$route.params.activityId);
+        this.updateUserContinuousActivities(this.user.cont_activities);
       }
     }
   }
