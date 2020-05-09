1281d02d64963e239b0c1ae3bf6ef2e5def6815e
Ryosuke Okamae <rok17@uclive.ac.nz>
2020-05-09 22:16:39 +1200

    Modify typo and apply code formatter to follow our code policy #story[249]

--- client/src/components/profile/settings/ProfileActivityTypeSettings.vue
+++ client/src/components/profile/settings/ProfileActivityTypeSettings.vue
@@ -2,9 +2,9 @@
   <div class="settingsContainer">
     <UserSettingsMenu />
     <div class="settingsContentContainer">
-      <h1>Edit Activities</h1>
-      <hr>
-      <div class="itemContainer"  v-for="activity in searchedUser.activities" v-bind:key="activity">
+      <h1>Edit Activity Type</h1>
+      <hr />
+      <div class="itemContainer" v-for="activity in searchedUser.activities" v-bind:key="activity">
         <h4>{{activity}}</h4>
         <button class="genericDeleteButton" v-on:click="removeActivityType(activity)">remove</button>
         <div class="floatClear"></div>
@@ -12,17 +12,18 @@
       <div id="activityActions">
         <form class="formTopSpacing" @submit.prevent>
           <select
-                  v-model="selected_activity"
-                  name="activityType"
-                  placeholder="Activity Type"
-                  value="Activity Type"
-                  class="itemSelect"
-                  required
+            v-model="selected_activity"
+            name="activityType"
+            placeholder="Activity Type"
+            value="Activity Type"
+            class="itemSelect"
+            required
           >
             <option selected disabled hidden>Activity Type</option>
-            <option v-for="addingActivity in activities_option" v-bind:key="addingActivity">
-              {{addingActivity}}
-            </option>
+            <option
+              v-for="addingActivity in activities_option"
+              v-bind:key="addingActivity"
+            >{{addingActivity}}</option>
           </select>
           <button class="genericConfirmButton addItemButton" v-on:click="addActivityType()">Add</button>
           <button class="genericConfirmButton saveButton" v-on:click="saveActivityTypes()">Save</button>
@@ -35,112 +36,130 @@
 </template>
 
 <script>
-  import UserSettingsMenu from "./ProfileSettingsMenu";
-  import {mapActions, mapState, mapGetters} from 'vuex'
-  import {apiUser} from "../../../api";
+import UserSettingsMenu from "./ProfileSettingsMenu";
+import { mapActions, mapState, mapGetters } from "vuex";
+import { apiUser } from "../../../api";
 
-  export default {
-    components: {
-      UserSettingsMenu
-    },
-    data() {
-      return {
-        selected_activity: "Activity Type",
-        activities_option: [],
-        num_of_activities: 1,
-        searchedUser: {}
-      }
-    },
-    computed: {
-      ...mapState(['user']),
-      ...mapGetters(['user'])
-    },
-    created: async function() {
-        // Ensures only activity types from the database can be selected and cannot select ones already selected
-        await apiUser.getActivityTypes().then((response) => {
+export default {
+  components: {
+    UserSettingsMenu
+  },
+  data() {
+    return {
+      selected_activity: "Activity Type",
+      activities_option: [],
+      num_of_activities: 1,
+      searchedUser: {}
+    };
+  },
+  computed: {
+    ...mapState(["user"]),
+    ...mapGetters(["user"])
+  },
+  created: async function() {
+    // Ensures only activity types from the database can be selected and cannot select ones already selected
+    await apiUser
+      .getActivityTypes()
+      .then(response => {
         const activityTypes = response.data;
 
-        for(let activity of this.searchedUser.activities) {
+        for (let activity of this.searchedUser.activities) {
           const index = activityTypes.indexOf(activity);
           if (index === -1) continue;
-          activityTypes.splice(index, 1)
+          activityTypes.splice(index, 1);
         }
         this.activities_option = activityTypes;
-      }).catch(error => console.log(error))
+      })
+      .catch(error => console.log(error));
+  },
+  mounted() {
+    this.loadSearchedUser();
+  },
+  methods: {
+    ...mapActions(["updateActivities"]),
+
+    startUp() {
+      console.log("init");
+      this.searchedUser.activities = this.searchedUser.activities.slice();
     },
-    mounted() {
-        this.loadSearchedUser()
+
+    /**
+     * Adds an activity type to the searchedUser on add button click
+     */
+    addActivityType() {
+      if (!this.selected_activity || this.selected_activity === "Activity Type")
+        return;
+      this.searchedUser.activities.push(this.selected_activity);
+      console.log(this.searchedUser.activities);
+      const index = this.activities_option.indexOf(this.selected_activity);
+      if (index === -1) return;
+      this.activities_option.splice(index, 1);
+      this.selected_activity = "Activity Type";
+      this.updateActivities(this.searchedUser);
     },
-    methods: {
-      ...mapActions(['updateActivities']),
 
-      startUp() {
-        console.log('init');
-        this.searchedUser.activities = this.searchedUser.activities.slice()
-      },
+    /**
+     * Removes an activity from the searchedUser on remove button click
+     * @param activity to remove
+     */
+    removeActivityType(activity) {
+      const index = this.searchedUser.activities.indexOf(activity);
+      if (index === -1) return;
+      this.searchedUser.activities.splice(index, 1);
+      this.activities_option.push(activity);
+      this.activities_option.sort();
+      this.updateActivities(this.searchedUser);
+    },
 
-      /**
-       * Adds an activity type to the searchedUser on add button click
-       */
-      addActivityType(){
-        if(!this.selected_activity || this.selected_activity === "Activity Type") return;
-        this.searchedUser.activities.push(this.selected_activity);
-        console.log(this.searchedUser.activities);
-        const index = this.activities_option.indexOf(this.selected_activity);
-        if(index === -1) return;
-        this.activities_option.splice(index, 1);
-        this.selected_activity = "Activity Type";
-        this.updateActivities(this.searchedUser)
-      },
-
-      /**
-       * Removes an activity from the searchedUser on remove button click
-       * @param activity to remove
-       */
-      removeActivityType(activity) {
-        const index = this.searchedUser.activities.indexOf(activity);
-        if (index === -1) return;
-        this.searchedUser.activities.splice(index, 1);
-        this.activities_option.push(activity);
-        this.activities_option.sort();
-        this.updateActivities(this.searchedUser)
-      },
-
-      /**
-       * Sends update request for all activity type changes to server on save button click
-       */
-      saveActivityTypes() {
-        this.updateActivities(this.searchedUser);
-        console.log(this.searchedUser.activities);
-        apiUser.editUserActivityTypes(this.searchedUser.profile_id, this.searchedUser.activities).then((response) => {
-          document.getElementById("activity_type_success").hidden = false;
-          document.getElementById("activity_type_error").hidden = true;
-          console.log(response);
-        }, (error) => {
-          document.getElementById("activity_type_error").hidden = false;
-          document.getElementById("activity_type_error").innerText = error.response.data.Errors;
-          document.getElementById("activity_type_success").hidden = true;
-          console.log(error);
-        });
+    /**
+     * Sends update request for all activity type changes to server on save button click
+     */
+    saveActivityTypes() {
+      this.updateActivities(this.searchedUser);
+      console.log(this.searchedUser.activities);
+      apiUser
+        .editUserActivityTypes(
+          this.searchedUser.profile_id,
+          this.searchedUser.activities
+        )
+        .then(
+          response => {
+            document.getElementById("activity_type_success").hidden = false;
+            document.getElementById("activity_type_error").hidden = true;
+            console.log(response);
+          },
+          error => {
+            document.getElementById("activity_type_error").hidden = false;
+            document.getElementById("activity_type_error").innerText =
+              error.response.data.Errors;
+            document.getElementById("activity_type_success").hidden = true;
+            console.log(error);
+          }
+        );
     },
 
     /*
         Uses user id from url to request user data.
      */
     async loadSearchedUser() {
-        if(this.$route.params.profileId == null || this.$route.params.profileId == ""){
-            this.$router.push('/settings/activities/'+this.user.profile_id);
-            this.searchedUser = this.user;
-        }else{
-            var tempUserData = await apiUser.getUserById(this.$route.params.profileId);
-            if(tempUserData == "Invalid permissions"){
-                this.$router.push('/settings/activities/'+this.user.profile_id);
-                this.searchedUser = this.user;
-            }else{
-                this.searchedUser = tempUserData;
-            }
+      if (
+        this.$route.params.profileId == null ||
+        this.$route.params.profileId == ""
+      ) {
+        this.$router.push("/settings/activities/" + this.user.profile_id);
+        this.searchedUser = this.user;
+      } else {
+        var tempUserData = await apiUser.getUserById(
+          this.$route.params.profileId
+        );
+        if (tempUserData == "Invalid permissions") {
+          this.$router.push("/settings/activities/" + this.user.profile_id);
+          this.searchedUser = this.user;
+        } else {
+          this.searchedUser = tempUserData;
         }
-        this.startUp();
+      }
+      this.startUp();
     }
   }
 };
--- client/src/components/profile/settings/ProfileSettingsMenu.vue
+++ client/src/components/profile/settings/ProfileSettingsMenu.vue
@@ -1,30 +1,40 @@
 <template>
-    <div class="settingsNavContainer">
-      <NavBar/>
-      <h1>Settings</h1>
-      <div class="navButtonContainer">
-        <router-link  v-bind:to="'/settings/profile/' + this.$route.params.profileId"><div class="settingsNavButton">Profile</div></router-link>
-        <hr>
-        <router-link v-bind:to="'/settings/email/' + this.$route.params.profileId"><div class="settingsNavButton">Emails</div></router-link>
-        <hr>
-        <router-link v-bind:to="'/settings/password/' + this.$route.params.profileId"><div class="settingsNavButton">Password</div></router-link>
-        <hr>
-        <router-link v-bind:to="'/settings/passport_countries/' + this.$route.params.profileId"><div class="settingsNavButton">Passport Countries</div></router-link>
-        <hr>
-        <router-link v-bind:to="'/settings/activities/' + this.$route.params.profileId"><div class="settingsNavButton">Activities</div></router-link>
-      </div>
+  <div class="settingsNavContainer">
+    <NavBar />
+    <h1>Settings</h1>
+    <div class="navButtonContainer">
+      <router-link v-bind:to="'/settings/profile/' + this.$route.params.profileId">
+        <div class="settingsNavButton">Profile</div>
+      </router-link>
+      <hr />
+      <router-link v-bind:to="'/settings/email/' + this.$route.params.profileId">
+        <div class="settingsNavButton">Emails</div>
+      </router-link>
+      <hr />
+      <router-link v-bind:to="'/settings/password/' + this.$route.params.profileId">
+        <div class="settingsNavButton">Password</div>
+      </router-link>
+      <hr />
+      <router-link v-bind:to="'/settings/passport_countries/' + this.$route.params.profileId">
+        <div class="settingsNavButton">Passport Countries</div>
+      </router-link>
+      <hr />
+      <router-link v-bind:to="'/settings/activities/' + this.$route.params.profileId">
+        <div class="settingsNavButton">Activitie Types</div>
+      </router-link>
     </div>
+  </div>
 </template>
 
 <script>
-  import NavBar from "../../modules/NavBar";
-  export default {
-    components: {
-        NavBar
-    },
+import NavBar from "../../modules/NavBar";
+export default {
+  components: {
+    NavBar
   }
+};
 </script>
 
 <style scoped>
-  @import '../../../../public/styles/pages/profileSettingsStyle.css';
+@import "../../../../public/styles/pages/profileSettingsStyle.css";
 </style>
