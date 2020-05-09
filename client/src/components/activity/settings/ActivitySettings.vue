dcc21d92d0a19bfe4dee05dcf22d9cb4d6ab2153
Samuel Sandri <sjs234@uclive.ac.nz>
2020-05-09 15:04:14 +1200

    Refactored activity settings to be clearer
    Refactored activity settings to be clearer. Extracted more methods out of addActivity method. Added more documentation.
    
    #story[253]

--- client/src/components/activity/settings/ActivitySettings.vue
+++ client/src/components/activity/settings/ActivitySettings.vue
@@ -96,221 +96,231 @@ import NavBar from "../../modules/NavBar";
 const COUNTRIES_URL = "https://restcountries.eu/rest/v2/all";
 
 export default {
-  components: {
-    NavBar
-  },
-  data() {
-    return {
-      selected_activity: "Activity Type",
-      activities_option: [],
-      countries_option: [],
-      adding_country: "Countries",
-      duration: "duration",
-      activity_types_selected: [],
-      start_date: null,
-      end_date: null,
-      start_time: null,
-      end_time: null
-    };
-  },
-  computed: {
-    ...mapGetters(["activity"]),
-    ...mapGetters(["user"])
-  },
-  created: async function() {
-    // Ensures only activity types from the database can be selected and cannot select ones already selected
-    await apiUser
-      .getActivityTypes()
-      .then(response => {
-        this.activities_option = response.data;
-      })
-      .catch(error => console.log(error));
-    // Gets list of countries that can be selected
-    await axios
-      .get(COUNTRIES_URL)
-      .then(response => {
-        const countries = [];
-        const data = response.data;
-        for (let country in data) {
-          let country_name = data[country].name;
-          countries.push(country_name);
-        }
-        this.countries_option = countries;
-      })
-      .catch(error => console.log(error));
-  },
-  methods: {
-    ...mapActions(["createActivity"]),
-    ...mapActions(["updateUserContinuousActivities"]),
-    ...mapActions(["updateUserDurationActivities"]),
-
-    /**
-     * Shows/hides date and time selection if duration is duration/continuous
-     */
-    setDuration() {
-      if (this.duration === "duration") {
-        document.getElementById("start_date").type = "date";
-        document.getElementById("end_date").type = "date";
-        document.getElementById("start_time").type = "time";
-        document.getElementById("end_time").type = "time";
-        document.getElementById("startDateLabel").hidden = false;
-        document.getElementById("endDateLabel").hidden = false;
-        document.getElementById("startTimeLabel").hidden = false;
-        document.getElementById("endTimeLabel").hidden = false;
-      } else {
-        document.getElementById("start_date").type = "hidden";
-        document.getElementById("end_date").type = "hidden";
-        document.getElementById("start_time").type = "hidden";
-        document.getElementById("end_time").type = "hidden";
-        document.getElementById("startDateLabel").hidden = true;
-        document.getElementById("endDateLabel").hidden = true;
-        document.getElementById("startTimeLabel").hidden = true;
-        document.getElementById("endTimeLabel").hidden = true;
-      }
+    components: {
+        NavBar
     },
-    /**
-     * Adds activity type to selected options
-     */
-    selectActivityType() {
-      if (this.selected_activity !== undefined) {
-        this.activity_types_selected.push(this.selected_activity);
-        let index = this.activities_option.indexOf(this.selected_activity);
-        if (index !== -1) {
-          this.activities_option.splice(index, 1);
-        }
-      }
+    data() {
+        return {
+            selected_activity: "Activity Type",
+            activities_option: [],
+            countries_option: [],
+            adding_country: "Countries",
+            duration: "duration",
+            activity_types_selected: [],
+            start_date: null,
+            end_date: null,
+            start_time: null,
+            end_time: null,
+            combinedStartTime: null,
+            combinedEndTime: null
+        };
     },
-    /**
-     * Removes activity type from selection
-     */
-    removeActivityType(addedActivity) {
-      this.activities_option.push(addedActivity);
-      let index = this.activity_types_selected.indexOf(addedActivity);
-      if (index !== -1) {
-        this.activity_types_selected.splice(index, 1);
-      }
+    computed: {
+        ...mapGetters(["activity"]),
+        ...mapGetters(["user"])
     },
-    /**
-     * Checks form conditions and sends create activity request if conditions pass
-     */
-    addActivity() {
-      let currentDate = new Date(Date.now());
-      let timeZone = currentDate
-        .toString()
-        .slice(currentDate.toString().indexOf("+"), 5);
-      if (this.activity.name === null || this.activity.name.trim() === "") {
-        this.displayError("Please select an activity name.");
-      } else if (
-        this.duration !== "duration" &&
-        this.duration !== "continuous"
-      ) {
-        this.displayError("Please select a duration.");
-      } else if (
-        this.duration === "duration" &&
-        (this.start_date === null ||
-          this.end_date === null ||
-          this.start_date === "" ||
-          this.end_date === "")
-      ) {
-        this.displayError("Please select start and end date.");
-      } else if (this.activity_types_selected.length < 1) {
-        this.displayError("Please select at least one activity type.");
-      } else {
-        let combinedStartTime;
-        let combinedEndTime;
-        if (this.duration !== "duration") {
-          combinedStartTime = null;
-          combinedEndTime = null;
-        } else if (this.start_time === null && this.end_time === null) {
-          combinedStartTime = this.start_date + "T00:00:00" + timeZone;
-          combinedEndTime = this.end_date + "T00:00:00" + timeZone;
-        } else if (this.start_time === null) {
-          combinedStartTime = this.start_date + "T00:00:00" + timeZone;
-          combinedEndTime =
-            this.end_date + "T" + this.end_time + ":00" + timeZone;
-        } else if (this.end_time === null) {
-          combinedStartTime =
-            this.start_date + "T" + this.start_time + ":00" + timeZone;
-          combinedEndTime = this.end_date + "T00:00:00" + timeZone;
-        } else {
-          combinedStartTime =
-            this.start_date + "T" + this.start_time + ":00" + timeZone;
-          combinedEndTime =
-            this.end_date + "T" + this.end_time + ":00" + timeZone;
-        }
+    created: async function () {
+        // Ensures only activity types from the database can be selected and cannot select ones already selected
+        await apiUser
+            .getActivityTypes()
+            .then(response => {
+                this.activities_option = response.data;
+            })
+            .catch(error => console.log(error));
+        // Gets list of countries that can be selected
+        await axios
+            .get(COUNTRIES_URL)
+            .then(response => {
+                const countries = [];
+                const data = response.data;
+                for (let country in data) {
+                    let country_name = data[country].name;
+                    countries.push(country_name);
+                }
+                this.countries_option = countries;
+            })
+            .catch(error => console.log(error));
+    },
+    methods: {
+        ...mapActions(["createActivity"]),
+        ...mapActions(["updateUserContinuousActivities"]),
+        ...mapActions(["updateUserDurationActivities"]),
 
-        currentDate.setHours(0);
-        currentDate.setMinutes(0);
-        currentDate.setSeconds(0);
-        currentDate.setMilliseconds(0);
-        if (new Date(combinedStartTime) > new Date(combinedEndTime)) {
-          this.displayError("End time must be after start time.");
-          return;
-        } else if (
-          currentDate > new Date(this.start_date + "T00:00:00" + timeZone)
-        ) {
-          this.displayError("Start date must be in the future.");
-          return;
-        } else if (
-          currentDate.getFullYear() + 2 <
-            new Date(this.start_date + "T00:00:00" + timeZone).getFullYear() ||
-          currentDate.getFullYear() + 2 <
-            new Date(this.end_date + "T00:00:00" + timeZone).getFullYear()
-        ) {
-          this.displayError("Must be less than 2 years in the future.");
-          return;
-        }
-
-        this.duration = this.duration !== "duration";
-
-        apiActivity
-          .addActivity(
-            this.user.profile_id,
-            this.activity.name,
-            this.duration,
-            combinedStartTime,
-            combinedEndTime,
-            this.activity.description,
-            this.adding_country,
-            this.activity_types_selected
-          )
-          .then(
-            response => {
-              document.getElementById("activity_success").hidden = false;
-              document.getElementById("activity_error").hidden = true;
-              console.log(response);
-              apiUser
-                .getUserContinuousActivities(this.user.profile_id)
-                .then(response => {
-                  this.updateUserContinuousActivities(response.data);
-                });
-              apiUser
-                .getUserDurationActivities(this.user.profile_id)
-                .then(response => {
-                  this.updateUserDurationActivities(response.data);
-                });
-              router.push("Profile");
-            },
-            error => {
-              document.getElementById("activity_error").hidden = false;
-              document.getElementById("activity_error").innerText =
-                error.response.data.error;
-              document.getElementById("activity_success").hidden = true;
-              console.log(error);
+        /**
+         * Shows/hides date and time selection if duration is duration/continuous
+         */
+        setDuration() {
+            if (this.duration === "duration") {
+                document.getElementById("start_date").type = "date";
+                document.getElementById("end_date").type = "date";
+                document.getElementById("start_time").type = "time";
+                document.getElementById("end_time").type = "time";
+                document.getElementById("startDateLabel").hidden = false;
+                document.getElementById("endDateLabel").hidden = false;
+                document.getElementById("startTimeLabel").hidden = false;
+                document.getElementById("endTimeLabel").hidden = false;
+            } else {
+                document.getElementById("start_date").type = "hidden";
+                document.getElementById("end_date").type = "hidden";
+                document.getElementById("start_time").type = "hidden";
+                document.getElementById("end_time").type = "hidden";
+                document.getElementById("startDateLabel").hidden = true;
+                document.getElementById("endDateLabel").hidden = true;
+                document.getElementById("startTimeLabel").hidden = true;
+                document.getElementById("endTimeLabel").hidden = true;
             }
-          );
-      }
-    },
-    /**
-     * Shows error text for given error string
-     * @param error
-     */
-    displayError(error) {
-      document.getElementById("activity_error").hidden = false;
-      document.getElementById("activity_error").innerText = error;
-      document.getElementById("activity_success").hidden = true;
+        },
+        /**
+         * Adds activity type to selected options
+         */
+        selectActivityType() {
+            if (this.selected_activity !== undefined) {
+                this.activity_types_selected.push(this.selected_activity);
+                let index = this.activities_option.indexOf(this.selected_activity);
+                if (index !== -1) {
+                    this.activities_option.splice(index, 1);
+                }
+            }
+        },
+        /**
+         * Removes activity type from selection
+         */
+        removeActivityType(addedActivity) {
+            this.activities_option.push(addedActivity);
+            let index = this.activity_types_selected.indexOf(addedActivity);
+            if (index !== -1) {
+                this.activity_types_selected.splice(index, 1);
+            }
+        },
+        /**
+         * Combines the times and dates given in the form to a single datetime format
+         * Sets datetime to null if continuous activity
+         */
+        combineDateTime() {
+            let currentDate = new Date(Date.now());
+            let timeZone = currentDate.toString().slice(currentDate.toString().indexOf("+"), 5);
+
+            if (this.duration !== "duration") {
+                this.combinedStartTime = null;
+                this.combinedEndTime = null;
+            } else if (this.start_time === null && this.end_time === null) {
+                this.combinedStartTime = this.start_date + "T00:00:00" + timeZone;
+                this.combinedEndTime = this.end_date + "T00:00:00" + timeZone;
+            } else if (this.start_time === null) {
+                this.combinedStartTime = this.start_date + "T00:00:00" + timeZone;
+                this.combinedEndTime =
+                    this.end_date + "T" + this.end_time + ":00" + timeZone;
+            } else if (this.end_time === null) {
+                this.combinedStartTime = this.start_date + "T" + this.start_time + ":00" + timeZone;
+                this.combinedEndTime = this.end_date + "T00:00:00" + timeZone;
+            } else {
+                this.combinedStartTime = this.start_date + "T" + this.start_time + ":00" + timeZone;
+                this.combinedEndTime = this.end_date + "T" + this.end_time + ":00" + timeZone;
+            }
+        },
+        /**
+         * Checks if the datetime passes conditions. Ensures time is in the future and start is not later than end
+         * @return boolean true if passes, false if fails
+         */
+        checkTimeContinuity() {
+            let currentDate = new Date(Date.now());
+            let timeZone = currentDate.toString().slice(currentDate.toString().indexOf("+"), 5);
+
+            currentDate.setHours(0);
+            currentDate.setMinutes(0);
+            currentDate.setSeconds(0);
+            currentDate.setMilliseconds(0);
+            if (new Date(this.combinedStartTime) > new Date(this.combinedEndTime)) {
+                this.displayError("End time must be after start time.");
+                return false;
+            } else if (currentDate > new Date(this.start_date + "T00:00:00" + timeZone)) {
+                this.displayError("Start date must be in the future.");
+                return false;
+            } else if (currentDate.getFullYear() + 2 < new Date(this.start_date + "T00:00:00" + timeZone).getFullYear()
+                || currentDate.getFullYear() + 2 < new Date(this.end_date + "T00:00:00" + timeZone).getFullYear()) {
+                this.displayError("Must be less than 2 years in the future.");
+                return false;
+            }
+            return true;
+        },
+        /**
+         * Check all activity form conditions
+         * @return boolean true if passes, false if fails
+         */
+        checkFormConditions() {
+            if (this.activity.name === null || this.activity.name.trim() === "") {
+                // Name is empty
+                this.displayError("Please select an activity name.");
+                return false;
+            } else if (this.duration !== "duration" && this.duration !== "continuous") {
+                // Duration is not set
+                this.displayError("Please select a duration.");
+                return false;
+            } else if (this.duration === "duration" &&
+                (this.start_date === null || this.end_date === null || this.start_date === "" || this.end_date === "")) {
+                // Start or end date not set
+                this.displayError("Please select start and end date.");
+                return false;
+            } else if (this.activity_types_selected.length < 1) {
+                // No activity types selected
+                this.displayError("Please select at least one activity type.");
+                return false;
+            } else if (this.duration === "duration" && !this.checkTimeContinuity()) {
+                // Time check failed
+                return false;
+            } else {
+                // All passed
+                return true;
+            }
+        },
+        /**
+         * Checks form conditions and sends create activity request if conditions pass
+         */
+        addActivity() {
+            // Combines dates and times, must be done before checking form
+            this.combineDateTime();
+
+            // Checks all activity attribute conditions
+            if (!this.checkFormConditions()) {
+                return;
+            }
+
+            // Sets duration to a boolean for the request
+            this.duration = this.duration !== "duration";
+
+            // Send a create request
+            apiActivity.addActivity(this.user.profile_id, this.activity.name, this.duration, this.combinedStartTime,
+                this.combinedEndTime, this.activity.description, this.adding_country, this.activity_types_selected)
+                .then(
+                    response => {
+                        document.getElementById("activity_success").hidden = false;
+                        document.getElementById("activity_error").hidden = true;
+                        console.log(response);
+                        apiUser.getUserContinuousActivities(this.user.profile_id)
+                            .then(response => {
+                                this.updateUserContinuousActivities(response.data);
+                            });
+                        apiUser.getUserDurationActivities(this.user.profile_id)
+                            .then(response => {
+                                this.updateUserDurationActivities(response.data);
+                            });
+                        router.push("Profile");
+                    },
+                    error => {
+                        this.displayError(error.data.error);
+                        console.log(error);
+                    });
+        },
+        /**
+         * Shows error text for given error string
+         * @param error
+         */
+        displayError(error) {
+            document.getElementById("activity_error").hidden = false;
+            document.getElementById("activity_error").innerText = error;
+            document.getElementById("activity_success").hidden = true;
+        }
     }
-  }
 };
 </script>
 
