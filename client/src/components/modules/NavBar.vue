c2bb7c289dc755a6064ad50a6b2ae870d93e9eb6
Karna Malone <kkm22@uclive.ac.nz>
2020-05-29 10:46:19 +1200

    Added search button to navbar.
    #story[312]

--- client/src/components/modules/NavBar.vue
+++ client/src/components/modules/NavBar.vue
@@ -8,6 +8,10 @@
       <button class="myaccount navBarButton">Profile</button>
     </router-link>
 
+    <router-link v-bind:to="'/profile/search'" v-if="user.isLogin">
+      <button class="myaccount navBarButton" id="navBarSearchBtn"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>Search</button>
+    </router-link>
+
     <router-link v-bind:to="'/settings/admin_dashboard'" v-if="isAdmin && user.isLogin">
       <button class="navBarButton" >Dashboard</button>
     </router-link>
@@ -15,6 +19,7 @@
     <router-link to="/signup" v-if="!user.isLogin">
       <button class="signup navBarButton" name="Sign Up">Sign Up</button>
     </router-link>
+
     <router-link to="/login" v-if="!user.isLogin">
       <button class="login navBarButton" value="Login In">Login</button>
     </router-link>
