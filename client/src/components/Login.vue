14f7b3f88ee50d3687d9de9429b057c8f651d59d
Samuel Sandri <sjs234@uclive.ac.nz>
2020-07-19 15:45:20 +1200

    Added loading indicators to login and email settings
    Added loading indicators to login and email settings. Converted email settings to vuetify.
    
    #story[329]

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -26,7 +26,15 @@
                     </div>
                     <hr>
                     <div class="loginRow">
-                        <button type="submit" v-on:click="submitLogin()" class="loginButton"> Login</button>
+                        <v-btn v-on:click="submitLogin()" class="loginButton"
+                               :loading="loadingLogin"
+                               :disabled="loadingLogin"
+                               color="#1cca92"
+                               outlined
+                               rounded
+                        >
+                            Login
+                        </v-btn>
                     </div>
                 </form>
             </div>
@@ -50,7 +58,7 @@
         topErrorMsg: "",
         passwordErrorMsg: "",
         otherErrorMsg: "",
-
+        loadingLogin: false,
       }
     },
     components: {
@@ -72,9 +80,11 @@
         this.topErrorMsg = "";
         this.passwordErrorMsg = "";
         this.otherErrorMsg = "";
+        this.loadingLogin = true;
 
         if (!this.user.primary_email || !this.user.password) {
           this.topErrorMsg = "Please enter email or password";
+          this.loadingLogin = false;
           return;
         }
         if (this.user.primary_email.trim(), this.user.password.trim()) {
@@ -96,9 +106,9 @@
               }
             })
             .catch(error => {
-
               const responseData = error.response.data;
               const responseCode = error.response.status;
+              this.loadingLogin = false;
 
               if (responseCode === 403 && responseData === "Email does not exist") {
                 this.topErrorMsg = "Account does not exist"
