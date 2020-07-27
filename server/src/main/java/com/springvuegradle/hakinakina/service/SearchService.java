d4296083f0a723a44f0815fcfe52a2ee708dfee8
Frankie <fop12@uclive.ac.nz>
2020-07-27 23:10:25 +1200

    Added a SearchService Class
    - Just set up the structure, haven't moved search endpoints yet
    - Fixed minor documentation
    
    #story[370]

--- server/src/main/java/com/springvuegradle/hakinakina/service/SearchService.java
+++ server/src/main/java/com/springvuegradle/hakinakina/service/SearchService.java
@@ -1,35 +1,33 @@
-package com.springvuegradle.hakinakina.controller;
+package com.springvuegradle.hakinakina.service;
 
 import com.springvuegradle.hakinakina.repository.ActivityTypeRepository;
 import com.springvuegradle.hakinakina.repository.EmailRepository;
 import com.springvuegradle.hakinakina.repository.SessionRepository;
 import com.springvuegradle.hakinakina.repository.UserRepository;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Set;
+import org.springframework.stereotype.Service;
 
 /**
- * Rest controller class for controlling requests related to searching
+ * Service class for search request actions
  */
-@RestController
-public class SearchController {
+@Service
+public class SearchService {
     public UserRepository userRepository;
     public EmailRepository emailRepository;
     public SessionRepository sessionRepository;
     public ActivityTypeRepository activityTypeRepository;
 
     /**
-     * Constructs a Search Controller, passing in the repositories and service so that they can be accessed.
+     * Constructs a Search Service, passing in the repositories and service so that they can be accessed.
      *
      * @param userRepository         The repository containing Users
      * @param emailRepository        The repository containing Emails
      * @param sessionRepository      The repository containing Sessions
      * @param activityTypeRepository The repository containing Activities
      */
-    public SearchController(UserRepository userRepository,
-                            EmailRepository emailRepository,
-                            SessionRepository sessionRepository,
-                            ActivityTypeRepository activityTypeRepository) {
+    public SearchService(UserRepository userRepository,
+                         EmailRepository emailRepository,
+                         SessionRepository sessionRepository,
+                         ActivityTypeRepository activityTypeRepository) {
         this.userRepository = userRepository;
         this.emailRepository = emailRepository;
         this.sessionRepository = sessionRepository;
