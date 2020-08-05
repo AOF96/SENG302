c44aaab6e3299ad6a8ce8453ec599361ddeefb32
Shivin Gaba <sga113@uclive.ac.nz>
2020-08-05 15:55:20 +1200

    Started implementing the end points for getting the all the changes done to an activity.
    #story[373] #pair[sga113, kkm22]

--- client/src/api.js
+++ client/src/api.js
@@ -229,6 +229,8 @@ export const apiActivity = {
     }),
 
   getActivity: (activityId) => instance.get(`/activities/${activityId}`),
+  getActivityUpdates: (activityId) => instance.get(`/activities/${activityId}/changes`),
+
 
   deleteActivity: (authorId, activityId) =>
     instance.delete(`/profiles/${authorId}/activities/${activityId}`),
