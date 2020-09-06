3bb7ee8706a79cca6fd6c80c76e5d6c13e63e802
Samuel Sandri <sjs234@uclive.ac.nz>
2020-09-06 13:26:13 +1200

    Added map page with map
    Added map page with full page map.
    
    #story[446]

---
+++ client/src/components/map/Map.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="map">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Map",
+    data: function() {
+      return {
+        map: null,
+      }
+    },
+    mounted() {
+      this.map = new window.google.maps.Map(document.getElementById("map"), {
+        center: {
+          lat: -34.397,
+          lng: 150.644
+        },
+        zoom: 8
+      });
+    }
+  }
+</script>
+
+<style scoped>
+  @import "../../../public/styles/pages/mapStyle.css";
+</style>
