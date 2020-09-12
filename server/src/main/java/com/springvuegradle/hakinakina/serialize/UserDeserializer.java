29f8f355d3525d31d8d127385938d49ef79d48b6
Samuel Sandri <sjs234@uclive.ac.nz>
2020-09-13 11:00:52 +1200

    Fix merge conflict

--- server/src/main/java/com/springvuegradle/hakinakina/serialize/UserDeserializer.java
+++ server/src/main/java/com/springvuegradle/hakinakina/serialize/UserDeserializer.java
@@ -122,7 +122,7 @@ public class UserDeserializer extends StdDeserializer<User> {
         if (node.get("location") != null) {
             Location location = createLocation(node.get("location"));
             locationRepository.save(location);
-            user.setHomeLocation(location);
+            user.setLocation(location);
         }
 
         return user;
