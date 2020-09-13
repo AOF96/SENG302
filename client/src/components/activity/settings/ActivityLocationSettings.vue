406eba82210fa9557199ce96dbe9993947e2abea
Jackie Qiu <jqi26@uclive.ac.nz>
2020-09-13 15:57:23 +1200

    Added code to ActivityLocationSettings.vue to parse the pin the user places on the map and updates the location data appropriately.
    #story[448] #pair[kkm22,jqi26]

--- client/src/components/activity/settings/ActivityLocationSettings.vue
+++ client/src/components/activity/settings/ActivityLocationSettings.vue
@@ -6,7 +6,7 @@
       <v-row justify="center" align="center"><h4>Select on map or type in the box</h4></v-row>
     </v-container>
     <div class="locationFieldDiv">
-      <v-text-field id="activityLocationAutocomplete" class="locationInput" label="Address" outlined dense></v-text-field>
+      <v-text-field v-model="address" id="activityLocationAutocomplete" class="locationInput" label="Address" outlined dense></v-text-field>
     </div>
   </div>
 </template>
@@ -18,6 +18,17 @@
     data() {
       return {
         marker: null,
+        location: {
+          street_address: null,
+          suburb: null,
+          postcode: null,
+          city: null,
+          state: null,
+          country: null,
+          latitude: null,
+          longitude: null,
+        },
+        address: ""
       }
     },
 
@@ -48,28 +59,33 @@
           mapTypeControl: false
         });
 
+        let outer = this;
+
         map.addListener('click', function (e) {
-          if (this.marker != null) {
-            this.marker.setMap(null);
+          if (outer.marker != null) {
+            outer.marker.setMap(null);
           }
-          this.marker = new window.google.maps.Marker({
+          outer.marker = new window.google.maps.Marker({
             position: e.latLng,
             map: map
           });
           map.panTo(e.latLng);
+          outer.getLocationFromLatLng(e.latLng);
         });
 
         this.setMapCentre(map);
         this.loadLocationAutocomplete();
       },
+
       /**
        * Centres the map on the users current location
        */
       setMapCentre(map) {
+        let outer = this;
         this.geocoder.geocode({'address': "Christchurch"}, function (results, status) {
           if (status === 'OK') {
             map.setCenter(results[0].geometry.location);
-            this.marker = new window.google.maps.Marker({
+            outer.marker = new window.google.maps.Marker({
               map: map,
               position: results[0].geometry.location
             });
@@ -80,6 +96,7 @@
           }
         });
       },
+
       /** load google autocomplete on create activity page **/
       loadLocationAutocomplete() {
         if (!window.google) {
@@ -91,7 +108,80 @@
               types: ["address"],
             }
         );
-      }
+      },
+
+      /**
+       * Extractor function that parses the google maps response and returns a location object.
+       */
+      extractLocationData(place) {
+        let newLocation = {street_address:"",suburb:"",postcode:"",city:"",state:"",country:"",latitude:"",longitude:""};
+        let addressComponents = place["address_components"];
+        newLocation["latitude"] = place["geometry"]["location"].lat();
+        newLocation["longitude"] = place["geometry"]["location"].lng();
+        let findingRoute = false;
+
+        for(let i = 0; i < addressComponents.length; i++){
+          let content = addressComponents[i]["long_name"];
+          if(addressComponents[i]["types"].includes("street_number")){newLocation.street_address = content+" ";findingRoute = true;}
+          if(addressComponents[i]["types"].includes("route")){
+            if(findingRoute){newLocation.street_address += content}else{newLocation.street_address = content}
+          }
+          if(addressComponents[i]["types"].includes("sublocality")){newLocation.suburb = content}
+          if(addressComponents[i]["types"].includes("locality")){newLocation.city = content}
+          if(addressComponents[i]["types"].includes("administrative_area_level_1")){newLocation.state = content}
+          if(addressComponents[i]["types"].includes("country")){newLocation.country = content}
+          if(addressComponents[i]["types"].includes("postal_code")){newLocation.postcode = content}
+        }
+
+        return newLocation;
+      },
+
+      /**
+       * Calls google maps api with lat lng and updates the location object
+       */
+      getLocationFromLatLng(latlng) {
+        let thisInner = this;
+        this.geocoder.geocode({'location': latlng}, function (results, status) {
+          if (status === 'OK') {
+            if(results.length == 0){
+              this.snackbarText = "Invalid Location";
+              this.snackbarColour = "error";
+              this.snackbar = true;
+            }else{
+              thisInner.location = thisInner.extractLocationData(results[0]);
+              thisInner.updateAddressString();
+            }
+          } else {
+            this.snackbarText = status;
+            this.snackbarColour = "error";
+            this.snackbar = true;
+          }
+        });
+      },
+
+      /**
+       * Updates the address string from the location object
+       */
+      updateAddressString() {
+        this.address = "";
+        if(this.location.street_address !== ""){this.address += this.location.street_address}
+        if(this.location.suburb !== ""){
+          if(this.address !== ""){this.address += ", "}
+          this.address += this.location.suburb;
+        }
+        if(this.location.city !== ""){
+          if(this.address !== ""){this.address += ", "}
+          this.address += this.location.city;
+        }
+        if(this.location.state !== ""){
+          if(this.address !== ""){this.address += ", "}
+          this.address += this.location.state;
+        }
+        if(this.location.country !== ""){
+          if(this.address !== ""){this.address += ", "}
+          this.address += this.location.country;
+        }
+      },
     }
   }
 </script>
