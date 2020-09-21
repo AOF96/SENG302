31d96d5cb6cb6cf10268a48d4aa048a119e037fb
Samuel Sandri <sjs234@uclive.ac.nz>
2020-09-22 10:17:26 +1200

    Fixed merge conflicts

--- server/src/main/java/com/springvuegradle/hakinakina/entity/Location.java
+++ server/src/main/java/com/springvuegradle/hakinakina/entity/Location.java
@@ -152,6 +152,13 @@ public class Location {
         return this.activity;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     @Override
     public boolean equals(Object o) {
