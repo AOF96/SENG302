065a603a116c0d503e6d911a2b24322c22435859
Frankie <fop12@uclive.ac.nz>
2020-09-21 23:55:58 +1200

    Added the legend for the map
    - Still needs styling because the icons are too big
    
    #story[445]

--- client/src/components/map/Map.vue
+++ client/src/components/map/Map.vue
@@ -1,6 +1,9 @@
 <template>
-  <div id="map">
-    <v-snackbar outlined color="error" :timeout="timeout" v-model="snackbar" bottom>{{errorMessage}}</v-snackbar>
+  <div>
+    <div id="map">
+      <v-snackbar outlined color="error" :timeout="timeout" v-model="snackbar" bottom>{{errorMessage}}</v-snackbar>
+    </div>
+    <div id="legend"><h3>Legend</h3></div>
   </div>
 </template>
 
@@ -40,6 +43,45 @@
           },
           zoom: 12
         });
+
+        var iconBase = 'https://i.imgur.com/';
+        var icons = {
+          publicActivity: {
+            name: 'Public',
+            icon: iconBase + 'MUWKzz9.png'
+          },
+          restrictedActivity: {
+            name: 'Restricted',
+            icon: iconBase + 'Y0JUUox.png'
+          },
+          privateActivity: {
+            name: 'Private',
+            icon: iconBase + 'lanhJgs.png'
+          },
+          publicActivityOwned: {
+            name: 'Public - Created',
+            icon: iconBase + 'Hz5QgGa.png'
+          },
+          restrictedActivityOwned: {
+            name: 'Restricted - Created',
+            icon: iconBase + '61rB4dm.png'
+          },
+          privateActivityOwned: {
+            name: 'Private - Created',
+            icon: iconBase + 'jNY9HSw.png'
+          }
+        };
+        var legend = document.getElementById('legend');
+        for (var key in icons) {
+          var type = icons[key];
+          var name = type.name;
+          var icon = type.icon;
+          var div = document.createElement('div');
+          div.innerHTML = '<img src="' + icon + '"> ' + name;
+          legend.appendChild(div);
+        }
+        [window.google.maps.ControlPosition.RIGHT_BOTTOM].push(document.getElementById('legend'));
+
         let address = this.user.location.street_address + ' ' + this.user.location.city + ' ' + this.user.location.country;
         this.geocoder.geocode({ 'address': address}, function(results, status) {
           if (status === 'OK') {
