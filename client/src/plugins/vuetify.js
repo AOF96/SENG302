d3a6bc6cf6648aa6932a2c8b399f0c94033a759f
Samuel Sandri <sjs234@uclive.ac.nz>
2020-09-22 15:50:46 +1200

    Added set of colours for vuetify themes
    Added set of colours for vuetify themes. Tested on css files.
    
    #story[468]

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -5,9 +5,36 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
     theme: {
+        dark: false,
+        options: {
+            customProperties: true,
+        },
         themes: {
             light: {
                 primary: '#1cca92',
+                secondary: '#ffffff',
+                background: '#ffffff',
+                component: '#ffffff',
+                primaryText: '#000000',
+                secondaryText: '#1cca92',
+                tertiaryText: '#ffffff',
+                success: '#4CAF50',
+                warning: '#FFC107',
+                error: '#FF5252',
+                info: '#2196F3',
+            },
+            dark: {
+                primary: '#1cca92',
+                secondary: '#424242',
+                background: '#222222',
+                component: '#424242',
+                primaryText: '#ffffff',
+                secondaryText: '#1cca92',
+                tertiaryText: '#222222',
+                success: '#4CAF50',
+                warning: '#FFC107',
+                error: '#FF5252',
+                info: '#2196F3',
             }
         }
     }
