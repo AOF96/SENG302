e990aab7a6dde53d03ef7b9c820851747471b06a
Samuel Sandri <sjs234@uclive.ac.nz>
2020-09-22 22:29:36 +1200

    Fixed tests
    Fixed failing tests by mocking call to vuetify object
    
    #story[468]

--- client/src/components/__test__/NavBarAdminLogin.spec.js
+++ client/src/components/__test__/NavBarAdminLogin.spec.js
@@ -11,6 +11,14 @@ localVue.use(VueRouter);
 const router = new VueRouter();
 localVue.use(Vuex);
 
+const mocks = {
+    $vuetify: {
+        theme: {
+            dark: true,
+        },
+    },
+};
+
 
 describe('NavBar after the login is successful', () => {
     let getters;
@@ -33,7 +41,7 @@ describe('NavBar after the login is successful', () => {
     });
 
     it('NavBar should have search, home, admin dashboard, profile and a settings button in the hamburger menu button', () => {
-        const wrapper = mount(NavBar, {store, localVue, router})
+        const wrapper = mount(NavBar, {store, localVue, router, mocks})
         expect(wrapper.find("#searchButton").exists()).toBe(true)
         expect(wrapper.find("#homeButton").exists()).toBe(true)
         expect(wrapper.find("#adminDashboardButton").exists()).toBe(true)
@@ -70,17 +78,17 @@ describe('NavBar after global admin login is successful', () => {
     });
 
     it('NavBar should not have any profile button for global admin', () => {
-        const wrapper = mount(NavBar, {store, localVue, router})
+        const wrapper = mount(NavBar, {store, localVue, router, mocks})
         expect(wrapper.find("#profileButton").exists()).toBe(false)
     });
 
     it('NavBar should not have any home button for global admin as that re-directs to the profile page for now', () => {
-        const wrapper = mount(NavBar, {store, localVue, router});
+        const wrapper = mount(NavBar, {store, localVue, router, mocks});
         expect(wrapper.find("#homeButton").exists()).toBe(false)
     });
 
     it('The hamburger menu has a Admin Dashboard button and clicking it takes the user to the Dashboard page.', () => {
-        const wrapper = mount(NavBar, {store, localVue, router});
+        const wrapper = mount(NavBar, {store, localVue, router, mocks});
         expect(wrapper.find("#adminDashboardButton").exists()).toBe(true);
         wrapper.find("#adminDashboardButton").trigger('click');
         expect(window.location.href).toBe('http://localhost/#/settings/admin_dashboard')
--- client/src/components/__test__/NavBarLogin.spec.js
+++ client/src/components/__test__/NavBarLogin.spec.js
@@ -1,5 +1,5 @@
 /* eslint-env jest*/
-import {createLocalVue, mount, shallowMount} from '@vue/test-utils'
+import {createLocalVue, mount} from '@vue/test-utils'
 import VueRouter from 'vue-router'
 import NavBar from '../modules/NavBar.vue'
 import Vuex from 'vuex'
@@ -8,6 +8,14 @@ const localVue = createLocalVue();
 localVue.use(VueRouter);
 const router = new VueRouter();
 localVue.use(Vuex);
+
+const mocks = {
+  $vuetify: {
+    theme: {
+      dark: true,
+    },
+  },
+};
 
 describe('NavBar after the login is successful', () => {
   let getters;
@@ -37,37 +45,37 @@ describe('NavBar after the login is successful', () => {
 
 
   it('NavBar should not have login button anymore and have website logo button instead', () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     expect(wrapper.find(".login").exists()).toBe(false);
     expect(wrapper.find("#appNavLogo").exists()).toBe(true)
 
   });
 
   it("Website's logo button that is on the NavBar on the click should redirects the user to the logged in user's news feed", () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     wrapper.find("#appNavLogo").trigger('click');
     expect(window.location.href).toBe('http://localhost/#/feed/')
   });
 
   it('Global search bar exists on the top of the NavBar', () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     expect(wrapper.find("#globalSearchBarInput").exists()).toBe(true)
   });
 
   it('A hamburger menu option exists for quick navigation around website in the of the NavBar', () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     expect(wrapper.find("#headerNavToggle").exists()).toBe(true)
   });
 
   it('The hamburger menu has a Home button and clicking it takes the user to the Home Feed.', () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     expect(wrapper.find("#homeButton").exists()).toBe(true);
     wrapper.find("#homeButton").trigger('click');
     expect(window.location.href).toBe('http://localhost/#/feed/')
   });
 
   it('The hamburger menu has a Map button and clicking it takes the user to the Map page.', () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     expect(wrapper.find("#hamburgerMap").exists()).toBe(true);
     wrapper.find("#hamburgerMap").trigger('click');
     expect(window.location.href).toBe('http://localhost/#/map/')
@@ -75,21 +83,21 @@ describe('NavBar after the login is successful', () => {
   });
 
   it('The hamburger menu has a Profile button and clicking it takes the user to the Profile page.', () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     expect(wrapper.find("#profileButton").exists()).toBe(true);
     wrapper.find("#profileButton").trigger('click');
     expect(window.location.href).toBe('http://localhost/#/profile/100')
   });
 
   it('The hamburger menu has a Search button and clicking it takes the user to the Search page.', () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     expect(wrapper.find("#searchButton").exists()).toBe(true);
     wrapper.find("#searchButton").trigger('click');
     expect(window.location.href).toBe('http://localhost/#/search')
   });
 
   it('The hamburger menu has a Settings button and clicking it takes the user to the Settings page.', () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     expect(wrapper.find("#settingsButton").exists()).toBe(true);
     wrapper.find("#settingsButton").trigger('click');
     expect(window.location.href).toBe('http://localhost/#/settings/profile/100')
@@ -97,12 +105,12 @@ describe('NavBar after the login is successful', () => {
 
 
   it("The hamburger menu displays the user's name John Smith.", () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     expect(wrapper.find("#hamburgerName").text()).toBe("John Smith")
   });
 
   it("The hamburger menu states that the user is Signed In.", () => {
-    const wrapper = mount(NavBar, {store, localVue, router});
+    const wrapper = mount(NavBar, {store, localVue, router, mocks});
     expect(wrapper.find("#hamburgerSignedIn").text()).toBe("Signed In")
   });
 
